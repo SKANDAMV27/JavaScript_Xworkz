fc4ec9ac60ff2bce1232edcca1beb27efafd7e1f
Skanda <skandagowda0@gmail.com>
2025-05-26 23:18:39 +0530

    Basic Code

---
+++ newFile.js
@@ -0,0 +1,27 @@
+var name='skanda'
+
+
+
+let emp2 = new Object();
+emp2.id=2;
+emp2.name='skanda';
+emp2.salary=30000;
+emp2.id=4
+function functionClick(){
+    console.log(name);
+    console.log(emp2);
+    let value = document.getElementsByTagName('p')[0].innerHTML;
+    console.log(value);
+    let value1 = document.getElementById('someID').innerHTML;
+    console.log(value1);
+    let name1 = document.getElementsByName('someClass')[0].innerHTML;
+    console.log(name1);
+
+}
+
+function clickFunction(){
+    let emp1 ={id:12,salary:45000,name:'arun'}
+    console.log(emp1);
+}
+
+
