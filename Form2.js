62680aece42c25a63ff2e139ae44c54d3bb018b7
Skanda <skandagowda0@gmail.com>
2025-06-10 23:43:22 +0530

    Form validiation

--- Form2.js
+++ Form2.js
@@ -1,34 +1,44 @@
 function validate() {
     let val = true;
-    let names = document.formName.nameName.value;
-    let emails = document.formName.emailEmail.value;
-    let passwods = document.formName.passwordPassword.value;
-    let conformpasswords = document.formName.conformpasswords.value;
 
-    let genders = document.formName.genderGender.value;
-    if (names.length > 10) {
-        alert('Form not get Submited')
+    let names = document.formName.nameName.value.trim();
+    let emails = document.formName.emailEmail.value.trim();
+    let passwords = document.formName.passwordPassword.value;
+    let confirmPasswords = document.formName.conformpasswords.value;
+
+    const gender = document.querySelector('input[name="genderGender"]:checked');
+    const programs = document.querySelectorAll('input[name="languageLanguage"]:checked');
+    const programs1 = Array.from(programs).map(e => e.value);
+    console.log('programs1',programs1)
+
+    if (names.length < 10) {
         val = false;
-
+        alert('Name must be at least 10 characters long.');
+    } else if (!emails.includes('@') || !emails.includes('.')) {
+        val = false;
+        alert('Invalid Email Address. It must include "@" and "."');
+    } else if (passwords.length < 8) {
+        val = false;
+        alert('Password must be at least 8 characters long.');
+    } else if (passwords !== confirmPasswords) {
+        val = false;
+        alert('Passwords do not match.');
+    } else if (selectedPrograms.length === 0) {
+        val = false;
+        alert('Please select at least one programming language.');
+    } else if (!gender) {
+        val = false;
+        alert('Please select your gender.');
+    } else {
+        alert(
+            'Form Submitted Successfully!\n\n' +
+            'Name: ' + names + '\n' +
+            'Email: ' + emails + '\n' +
+            'Password: ' + passwords + '\n' +
+            'Gender: ' + gender.value + '\n' +
+            'Languages: ' + selectedPrograms.join(', ')
+        );
     }
-
-    else if (!emails.include('@') || !emails.include('.')) {
-        alert('Invaid Email Address')
-        val = false;
-    }
-    else if (passwods.length < 8) {
-        alert('Password Must be more than 8 Characture')
-        val = false;
-
-    } else if (passwods !== conformpasswords) {
-        alert('Password not match')
-        val = false;
-
-    }
-    else
-        alert('Form Submitted Sucessfully')
 
     return val;
-
-
 }
